3c61ef5459ffcff97a683aac411ffa236c63d574
chensong <15850774503@163.com>
2024-03-26 15:45:00 +0800

    [测试工具]

---
+++ sip/stream_resolver/mutex.h
@@ -0,0 +1,46 @@
+/*
+**	********************************************************************************
+**
+**	File		: mutex.h
+**	Description	: 
+**	Modify		: 2020/4/28		zhangqiang		Create the file
+**	********************************************************************************
+*/
+#pragma once
+//#include <pthread.h>
+#include <mutex>
+
+namespace Zilu {
+namespace Infra {
+
+class CMutex
+{
+public:
+    CMutex();
+    ~CMutex();
+
+    int lock();
+
+    int unlock();
+
+    ///< 尝试加锁
+    ///<\return 0: mutex未加锁直接返回 EBUSY: mutex已加锁
+    int trylock();
+
+private:
+   // pthread_mutex_t     m_mtx;
+    std::mutex              m_mtx;
+};
+
+class CGuard
+{
+public:
+    CGuard(CMutex& m);
+    ~CGuard();
+
+private:
+    CMutex& m_mtx;
+};
+
+}
+}
